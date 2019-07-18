3d11310f16a5fc9833c28071a0c44f07ba5a58f0
Ubuntu <james@MyVirtualMachine.qxgkynm1nzuehmkppcwl5nba4d.zx.internal.cloudapp.net>
2019-07-18 08:48:18 +0000

    text & num gen running, but erroring

--- num-gen/app.py
+++ num-gen/app.py
@@ -6,16 +6,16 @@ from random import randint
 app = Flask(__name__)
 
 randcollector = ""
-@app.route(methods=['GET'])
+@app.route('/getNums', methods=['GET'])
 def num_gen_6(max):
     for x in range (0, 6):
         rand = randint(0, 9)
-    	randcollector += str(rand)
-	return jsonify({"Random Number":randcollector})
+        randcollector += str(rand)
+        return jsonify({"Random Number":randcollector})
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=9017)
+    app.run(host='0.0.0.0', port=9018)
--- text-gen/app.py
+++ text-gen/app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 randcollector = ""
 
-@app.route(methods=['GET'])
+@app.route('/getText', methods=['GET'])
 def text_gen_lower():
     for x in range (0, 3):
-    	rand = random.choice(string.ascii_lowercase)
-    	randcollector += rand
-    	return jsonify({"Random Characters":randcollector})
+        rand = random.choice(string.ascii_lowercase)
+        randcollector += rand
+        return jsonify({"Random Characters":randcollector})
 
 
 
@@ -24,4 +24,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=9017)
+    app.run(host='0.0.0.0', port=9019)
